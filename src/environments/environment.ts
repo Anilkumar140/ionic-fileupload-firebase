c323b0007f8c2d92e8760b9607f87670ef39b568
Anilkumrswain140@ <Anilkumrswain140@gmail.com>
2021-02-05 11:58:40 +0530

    1st commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// environments/environment.ts
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAY5mA0bBCFN1-eIxgNvw32neV-nrqHkis',
+    authDomain: 'nodejsapi-b6fd9.firebaseapp.com',
+    databaseURL: 'https://nodejsapi-b6fd9-default-rtdb.firebaseio.com/',
+    projectId: 'nodejsapi-b6fd9',
+    storageBucket: 'nodejsapi-b6fd9.appspot.com',
+    messagingSenderId: '82491772007',
+    appId: '1:82491772007:web:17b71a6386b823d3d958c2'
+  }
+};
